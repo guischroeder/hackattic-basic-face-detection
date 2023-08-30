0b6873dc346e530402b146132967ee12485dc696
guischroeder <guilherme.schroederr@gmail.com>
2023-08-30 01:45:02 -0300

    add solve problem service

--- controller/basic_face_detection_controller.go
+++ controller/basic_face_detection_controller.go
@@ -1,38 +1,22 @@
 package controller
 
 import (
-	"io"
 	"net/http"
 	"os"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 
 	"hackattic-basic-face-detection/libs/aws"
-	"hackattic-basic-face-detection/libs/aws/rekognition"
-	"hackattic-basic-face-detection/libs/aws/s3"
 	"hackattic-basic-face-detection/libs/hackattic"
+	"hackattic-basic-face-detection/libs/aws/rekognitionclient"
+	"hackattic-basic-face-detection/libs/aws/s3client"
+	"hackattic-basic-face-detection/libs/basicfacedetection"
 )
 
 func SolveProblem(context *gin.Context) {
-    hackatticManager := hackattic.HackatticClient{
+    hackatticClient := hackattic.HackatticClient{
         AccessToken: os.Getenv("HACKATTIC_ACCESS_TOKEN"),
     }
-
-    problem, err := hackatticManager.GetProblem()
-    if err != nil {
-        context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
-
-    resp, err := http.Get(problem.ImageUrl)
-    if err != nil {
-        context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
-
-    defer resp.Body.Close()
-
     session, err := aws.NewSession(
         os.Getenv("AWS_ACCESS_KEY_ID"),
         os.Getenv("AWS_SECRET_ACCESS_KEY"),
@@ -42,35 +26,13 @@ func SolveProblem(context *gin.Context) {
         context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
         return
     }
+    s3Client := s3client.S3(session)
+    rekognitionClient := rekognitionclient.Rekognition(session)
 
-    s3Client := s3.S3(session)
-    data, _ := io.ReadAll(resp.Body)
-    reader := strings.NewReader(string(data))
-
-    bucket := os.Getenv("S3_BUCKET_NAME")
-    path := "media/faces.jpg"
-    err = s3.Upload(s3Client, s3.UploadInput{
-        Bucket: bucket,
-        Path: path,
-        Image: io.ReadSeeker(reader), 
-    })
-    if err != nil {
-        context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
-
-    rekognitionClient := rekognition.Rekognition(session)
-    detectedFaces, err := rekognition.DetectFaces(
-        rekognitionClient, rekognition.S3Object{
-            Bucket: bucket,
-            Name: path,
-        },
+    solveProblem := basicfacedetection.NewSolveProblemService(
+        hackatticClient,
+        s3Client,
+        rekognitionClient,
     )
-
-    if err != nil {
-        context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
-
-    context.IndentedJSON(http.StatusOK, gin.H{"data": detectedFaces})
+    solveProblem.Perform()
 }
---
+++ libs/basicfacedetection/solve_problem_service.go
@@ -0,0 +1,76 @@
+package basicfacedetection
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"hackattic-basic-face-detection/libs/aws/rekognitionclient"
+	"hackattic-basic-face-detection/libs/aws/s3client"
+	"hackattic-basic-face-detection/libs/hackattic"
+
+	"github.com/aws/aws-sdk-go/service/rekognition"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+type HackatticClient interface {
+    GetProblem() (hackattic.Problem, error)
+}
+type SolveProblemService struct {
+    hackattic HackatticClient
+    s3Client *s3.S3
+    rekognitionClient *rekognition.Rekognition
+}
+
+func NewSolveProblemService(hackattic HackatticClient,
+    s3Client *s3.S3, rekognitionClient *rekognition.Rekognition) *SolveProblemService {
+    return &SolveProblemService{
+        hackattic: hackattic,
+        s3Client: s3Client,
+        rekognitionClient: rekognitionClient,
+    }
+}
+
+func (s *SolveProblemService) Perform() error {
+    problem, err := s.hackattic.GetProblem()
+    if err != nil {
+        return err
+    }
+
+    resp, err := http.Get(problem.ImageUrl)
+    if err != nil {
+        return err
+    }
+
+    defer resp.Body.Close()
+
+    data, _ := io.ReadAll(resp.Body)
+    reader := strings.NewReader(string(data))
+
+    bucket := os.Getenv("S3_BUCKET_NAME")
+    path := "media/faces.jpg"
+    err = s3client.Upload(s.s3Client, s3client.UploadInput{
+        Bucket: bucket,
+        Path: path,
+        Image: io.ReadSeeker(reader), 
+    })
+    if err != nil {
+        return err
+    }
+
+    detectedFaces, err := rekognitionclient.DetectFaces(
+        s.rekognitionClient, rekognitionclient.S3Object{
+            Bucket: bucket,
+            Name: path,
+        },
+    )
+    if err != nil {
+        return err
+    }
+
+    fmt.Println(detectedFaces)
+
+    return nil
+}
