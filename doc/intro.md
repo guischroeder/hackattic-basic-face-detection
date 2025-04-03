c542e28431192dcdefe0361bbc8e8385467f772e
guischroeder <guilherme.schroederr@gmail.com>
2025-04-03 12:17:53 -0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hackattic-basic-face-detection
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/hackattic_basic_face_detection/core_test.clj
@@ -0,0 +1,7 @@
+(ns hackattic-basic-face-detection.core-test
+  (:require [clojure.test :refer :all]
+            [hackattic-basic-face-detection.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
